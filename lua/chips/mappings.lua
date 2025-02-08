99b4644e9b5ba01687ab7e85bfebe86963183f3a
rcorreiaC <giga@fakemail.cum>
2025-02-08 18:50:17 +0000

    move mappings to mapping file and fix options not applying

--- lua/chips/mappings.lua
+++ lua/chips/mappings.lua
@@ -8,3 +8,11 @@ kmap("n", "<C-j>", "<C-w>j", opts)
 kmap("n", "<C-k>", "<C-w>k", opts)
 
 -- Explorer
+
+-- Keep visual after indenting
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
+
+
+-- vim.api.nvim_set_keymap('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true })
+
--- lua/chips/options.lua
+++ lua/chips/options.lua
@@ -1,54 +1,50 @@
+-- This options overwrites indent options based on the current filetype
+vim.api.nvim_command('filetype plugin indent off')
+
 -- :help options
---local options = {
-	-- autochdir = true, -- Automatically change the current directory based on current file.
-	-- autoindent = false,
-	-- autoread = true,
-	-- buflisted = true,
-	-- cindent = true,
-	-- cinkeys = "o,O", -- Keys that trigger auto-indent
-	-- clipboard = "unnamedplus",
-	-- cmdheight = 2,
-	-- completeopt = "menuone,noinsert",
-	-- conceallevel = 0,
-	-- confirm = true,
-	-- cursorline = true,
-	-- debug = "msg",
-	-- digraph = false,
-	-- display = "uhex",
-	-- expandtab = false,
-	-- fileencoding = "utf-8",
-	-- formatoptions = "tcnqjw",
-	-- hlsearch = true,
-	-- ignorecase = true,
-	-- indentexpr = "",
-	-- matchpairs = "(:),{:},[:],<:>",
-	-- mouse = "a",
-	-- number = true,
-	-- relativenumber = false,
-	-- scrolloff = 8,
-	-- shiftwidth = 2,
-	-- showtabline = 2,
-	-- sidescrolloff = 8,
-	-- signcolumn = "yes",
-	-- smartcase = false,
-	-- smartindent = false,
-	-- splitbelow = true,
-	-- splitright = true,
-	-- tabstop = 2,
-	-- updatetime = 300,
-	-- wrap = true,
-	-- scrolloff = 0,
-	-- winbar = "",
---}
+local options = {
+	autochdir = true, -- Automatically change the current directory based on current file.
+	autoindent = false,
+	autoread = true,
+	buflisted = true,
+	cindent = true,
+	cinkeys = "o,O", -- Keys that trigger auto-indent
+	clipboard = "unnamedplus",
+	cmdheight = 2,
+	completeopt = "menuone,noinsert",
+	conceallevel = 0,
+	confirm = true,
+	cursorline = true,
+	debug = "msg",
+	digraph = false,
+	display = "uhex",
+	expandtab = false,
+	fileencoding = "utf-8",
+	formatoptions = "tcnqjw",
+	hlsearch = true,
+	ignorecase = true,
+	indentexpr = "",
+	matchpairs = "(:),{:},[:],<:>",
+	mouse = "a",
+	number = true,
+	relativenumber = false,
+	scrolloff = 8,
+	shiftwidth = 2,
+	showtabline = 2,
+	sidescrolloff = 8,
+	signcolumn = "yes",
+	smartcase = false,
+	smartindent = true,
+	splitbelow = true,
+	splitright = true,
+	tabstop = 2,
+	updatetime = 300,
+	wrap = true,
+	scrolloff = 0,
+	winbar = "",
+}
 
---for k, v in pairs(options) do
---  vim.opt[k] = v
---end
-
--- Keep visual after indenting
---vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true })
---vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
-
-
--- vim.api.nvim_set_keymap('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true })
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
