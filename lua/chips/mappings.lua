dcca9e80e1b25336ef204187950c05c02b7d94db
rcorreia <rcorreiachips@gmail.com>
2025-03-01 18:42:19 +0000

    add keymappings

--- lua/chips/mappings.lua
+++ lua/chips/mappings.lua
@@ -2,28 +2,31 @@ local opts = { noremap = true, silent = true }
 local kmap = vim.api.nvim_set_keymap
 
 vim.g.mapleader = " "
-vim.cmd("filetype plugin off")
+-- vim.cmd("filetype plugin off")
 
 -- Window options
-kmap("n", "<C-h>", "<C-w>h", opts)
-kmap("n", "<C-l>", "<C-w>l", opts)
-kmap("n", "<C-j>", "<C-w>j", opts)
-kmap("n", "<C-k>", "<C-w>k", opts)
 kmap("n", "<C-s>", "<C-w><C-v>", opts)                          -- Open window
 kmap("n", "<C-q>", "<C-w>q", opts)                              -- Close window
 kmap("n", "<C-r>", "<C-w><C-r>", opts)                          -- Rotate window
 kmap("n", "<C-=>", "<C-w>=", opts)                              -- Make all windows same size (remove?)
 kmap("n", "<C-r>", "<C-w><C-r>", opts)                          -- Rotate window downwards and right
-kmap("n", "<C-left>", ":vertical res +1<CR>", opts)             -- Resize window vertically
-kmap("n", "<C-right>", ":vertical res -1<CR>", opts)            -- Resize window vertically
+kmap("n", "<C-left>", ":vertical res +3<CR>", opts)             -- Resize window vertically
+kmap("n", "<C-right>", ":vertical res -3<CR>", opts)            -- Resize window vertically
+kmap("n", "<C-down>", ":res -3<CR>", opts)            -- Resize window
+kmap("n", "<C-up>", ":res +3<CR>", opts)            -- Resize window 
 
 -- Explorer
-kmap("n", "<leader>e", ":Lexp<CR>", opts)
+kmap("n", "<leader>e", ":Neotree toggle<CR>", opts)
 
 -- Keep visual after indenting
-vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
+kmap("v", "<", "<gv", { noremap = true, silent = true })
+kmap("v", ">", ">gv", { noremap = true, silent = true })
 
+-- Remove highlight
+kmap("n", "<Esc>", ":noh<CR>", { noremap = true, silent = true })
+
+-- Do not jump to first when searching for word.
+kmap("n", "gd", "g*<C-o>", { noremap = true, silent = true })
 
 -- vim.api.nvim_set_keymap('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true })
 
